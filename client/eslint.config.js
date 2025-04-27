eb5a13968af73821f655c8d9753ea17386c06652
henryhoangduong <henryhoangduong@gmail.com>
2025-04-27 11:05:42 +0700

    login with password

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -18,11 +18,8 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
       'prettier/prettier': [
         'warn',
         {
