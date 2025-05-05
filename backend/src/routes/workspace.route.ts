3ffd84cbbaa2a637dbfce4439077f58d90ad77d5
henryhoangduong <henryhoangduong@gmail.com>
2025-05-05 11:50:55 +0700

    backend: update workspace controller

--- backend/src/routes/workspace.route.ts
+++ backend/src/routes/workspace.route.ts
@@ -2,7 +2,8 @@ import { Router } from 'express'
 import {
   createWorkspaceController,
   getAllWorkspacesUserIsMemberController,
-  getWorkspaceByIdController
+  getWorkspaceByIdController,
+  updateWorkspaceByIdController
 } from '../controllers/workspace.controller'
 
 export const workspaceRoutes = Router()
@@ -10,3 +11,4 @@ export const workspaceRoutes = Router()
 workspaceRoutes.post('/create/new', createWorkspaceController)
 workspaceRoutes.get('/all', getAllWorkspacesUserIsMemberController)
 workspaceRoutes.get('/:id', getWorkspaceByIdController)
+workspaceRoutes.put('/update/:id', updateWorkspaceByIdController)
