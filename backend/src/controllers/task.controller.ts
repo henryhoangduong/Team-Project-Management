c44315deceb6c6ca68cef6ee942cff170cd8605a
henryhoangduong <henryhoangduong@gmail.com>
2025-07-13 11:14:10 +0700

    backend: getAllTasks

--- backend/src/controllers/task.controller.ts
+++ backend/src/controllers/task.controller.ts
@@ -7,7 +7,7 @@ import { workspaceIdSchema } from '../validation/auth.validation'
 import { createTaskSchema, taskIdSchema, updateTaskSchema } from '../validation/task.validation'
 import { roleGuard } from '../utils/roleGuard'
 import { Permissions } from '../enums/role.enum'
-import { createTaskService, updateTaskService } from '../services/task.service'
+import { createTaskService, getAllTasksService, updateTaskService } from '../services/task.service'
 
 export const addTaskController = asyncHandler(async (req: Request, res: Response) => {
   const projectId = projectIdSchema.parse(req.params.projectId)
@@ -37,5 +37,32 @@ export const updateTaskController = asyncHandler(async (req: Request, res: Respo
   return res.status(HTTPSTATUS.OK).json({
     message: 'Task upated',
     updateTask
+  })
+})
+
+export const getAllTaskController = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.user?._id
+  const workspaceId = workspaceIdSchema.parse(req.params.workspaceId)
+
+  const filters = {
+    projectId: req.query.projectId as string | undefined,
+    priority: req.query.priority ? (req.query.priority as string)?.split(',') : undefined,
+    status: req.query.status ? (req.query.status as string)?.split(',') : undefined,
+    assignedTo: req.query.assignedTo ? (req.query.assignedTo as string)?.split(',') : undefined,
+    keyword: req.query.keyword as string | undefined,
+    dueDate: req.query.dueDate as string | undefined
+  }
+
+  const pagination = {
+    pageSize: parseInt(req.query.pageSize as string) || 10,
+    pageNumber: parseInt(req.query.pageNumber as string) || 1
+  }
+  const { role } = await getMemberRoleInWorkspace(userId, workspaceId)
+
+  roleGuard(role, [Permissions.VIEW_ONLY])
+  const result = await getAllTasksService(workspaceId, filters, pagination)
+  return res.status(HTTPSTATUS.OK).json({
+    message: 'Task upated',
+    result
   })
 })
--- backend/src/routes/task.route.ts
+++ backend/src/routes/task.route.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { addTaskController, updateTaskController } from '../controllers/task.controller'
+import { addTaskController, getAllTaskController, updateTaskController } from '../controllers/task.controller'
 
 export const taskRoutes = Router()
 
 taskRoutes.post('/project/:projectId/workspace/:workspaceId/create', addTaskController)
 taskRoutes.put('/:id/project/:projectId/workspace/:workspaceId/update', updateTaskController)
+taskRoutes.get('/workspace/:workspaceId/all', getAllTaskController)
